c5e1ce0425062374ff01f7f7a6e3018ed1a5d539
Brian Antunes <brian.antunes@yahoo.com>
2023-01-01 14:59:59 -0500

    Added README and cut down rest-api

--- src/rest-api.ts
+++ src/rest-api.ts
@@ -1,41 +1,39 @@
 import { Client, ThreadChannel } from "discord.js"
 import express from "express"
+import { Request, Response } from "express"
 
 export function createRestApi(client: Client) {
     const app = express()
     app.use(express.json())
 
     app.get("/messages", async (req, res) => {
-        const { threadId } = req.query
-        if(!threadId) {
-            return res.status(400).send("Missing threadId")
-        }
+        const { thread } = check(req, res)
 
-        const thread = await client.channels.fetch(threadId as string) as ThreadChannel
-        if(!thread) {
-            return res.status(404).send("Thread with this id was not found")
-        }
         const messages = await thread.messages.fetch()
         return res.status(200).send(JSON.stringify(messages || []))
     })
 
     app.post("/messages", async (req, res) => {
-        const { threadId, text } = req.body
-        if(!threadId) {
-            return res.status(400).send("Missing threadId")
-        }else if(!text) {
+        const { text } = req.body
+        const { thread } = check(req, res)
+
+        if(!text) {
             return res.status(400).send("Missing text")
         }
-
-        const thread = await client.channels.fetch(threadId as string) as ThreadChannel
-        if(!thread) {
-            return res.status(404).send("Thread with this id was not found")
-        }
+        
         await thread.send(text)
         return res.status(200).send("Message sent")
     })
 
     app.post("/resolve", async (req, res) => {
+        const { thread } = check(req, res)
+        
+        await thread.send("This conversation is marked as resolved and thread will be archived")
+        await thread.setArchived(true)
+        return res.status(200).send("Thread resolved")
+    })
+
+    async function check(req: Request, res: Response): ThreadChannel {
         const { threadId } = req.query
         if(!threadId) {
             return res.status(400).send("Missing threadId")
@@ -45,11 +43,9 @@ export function createRestApi(client: Client) {
         if(!thread) {
             return res.status(404).send("Thread with this id was not found")
         }
-        
-        await thread.send("This conversation is marked as resolved and thread will be archived")
-        await thread.setArchived(true)
-        return res.status(200).send("Thread resolved")
-    })
+
+        return thread
+    }
 
     return app
 }
