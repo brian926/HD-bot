88c713cdcec3131762a36f07c3c432999d24ca33
Brian Antunes <brian.antunes@yahoo.com>
2023-01-01 16:17:53 -0500

    Changed rest-api?

--- src/rest-api.ts
+++ src/rest-api.ts
@@ -1,39 +1,12 @@
 import { Client, ThreadChannel } from "discord.js"
 import express from "express"
-import { Request, Response } from "express"
+import { Response } from "express"
 
 export function createRestApi(client: Client) {
     const app = express()
     app.use(express.json())
 
     app.get("/messages", async (req, res) => {
-        const { thread } = check(req, res)
-
-        const messages = await thread.messages.fetch()
-        return res.status(200).send(JSON.stringify(messages || []))
-    })
-
-    app.post("/messages", async (req, res) => {
-        const { text } = req.body
-        const { thread } = check(req, res)
-
-        if(!text) {
-            return res.status(400).send("Missing text")
-        }
-        
-        await thread.send(text)
-        return res.status(200).send("Message sent")
-    })
-
-    app.post("/resolve", async (req, res) => {
-        const { thread } = check(req, res)
-        
-        await thread.send("This conversation is marked as resolved and thread will be archived")
-        await thread.setArchived(true)
-        return res.status(200).send("Thread resolved")
-    })
-
-    async function check(req: Request, res: Response): ThreadChannel {
         const { threadId } = req.query
         if(!threadId) {
             return res.status(400).send("Missing threadId")
@@ -43,8 +16,67 @@ export function createRestApi(client: Client) {
         if(!thread) {
             return res.status(404).send("Thread with this id was not found")
         }
+        const messages = await thread.messages.fetch()
+        return res.status(200).send(JSON.stringify(messages || []))
+    })
 
-        return thread
+    app.post("/messages", async (req, res) => {
+        const { threadId, text } = req.body
+        if(!threadId) {
+            return res.status(400).send("Missing threadId")
+        }else if(!text) {
+            return res.status(400).send("Missing text")
+        }
+
+        const thread = await client.channels.fetch(threadId as string) as ThreadChannel
+        if(!thread) {
+            return res.status(404).send("Thread with this id was not found")
+        }
+        await thread.send(text)
+        return res.status(200).send("Message sent")
+    })
+
+    type ThreadT = {
+        ThreadId: string,
+        Thread: ThreadChannel,
+        Res: Response
+    }
+
+    app.post("/resolve", async (req, res) => {
+        const { threadId } = req.query
+        // if(!threadId) {
+        //     return res.status(400).send("Missing threadId")
+        // }
+
+        // const thread = await client.channels.fetch(threadId as string) as ThreadChannel
+        // if(!thread) {
+        //     return res.status(404).send("Thread with this id was not found")
+        // }
+        const thread = checkT(threadId, res)
+
+        await (await thread).Thread.send("This conversation is marked as resolved and thread will be archived")
+        await (await thread).Thread.setArchived(true)
+        return (await thread).Res.send("Thread resolved")
+    })
+
+    async function checkT(threadId: any, res: Response) {
+        const resT = res.status(200)
+        if(!threadId) {
+            const resT = res.status(400).send("Missing threadId")
+        }
+        
+        const thread = await client.channels.fetch(threadId as string) as ThreadChannel
+        if(!thread) {
+            const resT = res.status(404).send("Thread with this id was not found")
+        }
+
+        const test: ThreadT = {
+            ThreadId: threadId,
+            Thread: thread,
+            Res: resT
+        }
+
+        return test
     }
 
     return app
